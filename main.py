12788f84099d246b94725911eb410e7bbc2f98c6
palinaprakapovich <156122022+palinaprakapovich@users.noreply.github.com>
2024-01-09 19:30:55 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+meme_dict = {"CRINGE": "Coś wyjątkowo dziwnego lub zawstydzającego",
+            "LOL": "Częsta reakcja na coś zabawnego", "ROFL": "odpowiedź na żart", "SHEESH": "lekka dezaprobata",
+            "CREEPY" :"straszny, złowieszczy", "AGGRO":"stać się agresywnym/zły"}
+word = input("Wpisz słowo, którego nie rozumiesz (używaj wielkich liter!): ") 
